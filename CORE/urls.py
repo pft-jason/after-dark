4dbefbcdf4e9bebabcbe42f34c3996d869390e49
jason <panelforgetechnologies@gmail.com>
2025-04-10 01:23:30 +1200

    Refactor authentication templates and styles for improved UI consistency and accessibility

--- CORE/urls.py
+++ CORE/urls.py
@@ -2,11 +2,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from GALLERY.views import gallery_home as default 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('gallery/', include('GALLERY.urls')),
     path('auth/', include('AUTH.urls')),
+    path('', default)
 ]
 
 if settings.DEBUG:
--- GALLERY/urls.py
+++ GALLERY/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', gallery_home, name="gallery"),    
+    path('', gallery_home, name="gallery_home"),    
 ]
